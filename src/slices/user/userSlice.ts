49491831e30c32e9fd8118321fa6a0c4bf2fbb4c
David Harush <davidharush@gmail.com>
2023-10-04 08:38:50 +0300

    save user data in store and change env to dev mode

--- src/slices/user/userSlice.ts
+++ src/slices/user/userSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AuthResponseType } from 'api/auth/auth.interface';
 
 import { UserState } from './user.interface';
+import { authApi } from 'api/auth/auth';
 
 export const userSlice = createSlice({
   initialState: {} as UserState,
@@ -11,6 +12,15 @@ export const userSlice = createSlice({
       state.user = action.payload.user;
       state.accessToken = action.payload.accessToken;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      authApi.endpoints.login.matchFulfilled,
+      (state, { payload }) => {
+        state.accessToken = payload.accessToken
+        state.user = payload.user
+      }
+    )
   },
 });
 
