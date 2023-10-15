34495029bb4e3c07c5a4daabd333ac8e1f05ddb2
David Harush <davidharush@gmail.com>
2023-10-15 20:29:11 +0300

    refactor store to a function with initial state

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,9 +1,10 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import { authApi } from 'api/auth/auth';
 import userReducer from 'slices/user/userSlice';
 
-const rootReducer = {
+const rootReducer = combineReducers({
   [authApi.reducerPath]: authApi.reducer,
   user: userReducer,
-};
+});
 
 export default rootReducer;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, PreloadedState } from '@reduxjs/toolkit';
 import { authApi } from 'api/auth/auth';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import rootReducer from './reducers';
-import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
-// ...
 
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApi.middleware]),
-});
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([authApi.middleware]),
+    preloadedState,
+  });
+}
 
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+const store = setupStore();
 export default store;
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
